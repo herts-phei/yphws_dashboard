6725eb012e1d513416fff29de3d0b978808d3a5c
ayu <Annie.Yu@hertfordshire.gov.uk>
2022-03-03 20:00:46 +0000

    comment out test chunk

--- app.R
+++ app.R
@@ -160,11 +160,11 @@ ui <- tablerDashPage(
 server <- function(input, output) {
   
   # Uncomment for testing
-  observe({
-    
-    if ("ethnicity" %in% input$comp) { browser() }
-    
-  })
+  # observe({
+  #   
+  #   if ("ethnicity" %in% input$comp) { browser() }
+  #   
+  # })
 
   # --Load all data-----
   rv <- reactiveValues()
