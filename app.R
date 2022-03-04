f5e046697c6ed7568a9ef35c765d9f984882d30f
ayu <Annie.Yu@hertfordshire.gov.uk>
2022-03-04 15:36:24 +0000

    barely-working version of report export functionality

--- app.R
+++ app.R
@@ -2,6 +2,7 @@
 ## IN DEVELOPMENT. 
 
 library(tidyverse)
+library(rmarkdown)
 library(pins)
 library(echarts4r)
 library(reactable)
@@ -141,6 +142,15 @@ ui <- tablerDashPage(
       ),
       tablerTabItem(
         tabName = "Export",
+        tagList(
+          fluidRow(
+            tablerCard(title = "Export full report",
+                       width = 12, 
+                       uiOutput("exp_report_comp"),
+                       uiOutput("exp_report_cat"),
+                       downloadButton("exp_report", "Export report"))
+          )
+        ),
         tablerCard(width = 12, title = "Data table",
                    reactableOutput("export")
         )
@@ -161,11 +171,11 @@ ui <- tablerDashPage(
 server <- function(input, output) {
   
   # Uncomment for testing
-  # observe({
-  #   
-  #   if ("ethnicity" %in% input$comp) { browser() }
-  #   
-  # })
+  observe({
+
+    if ("ethnicity" %in% input$comp) { browser() }
+
+  })
   
   # --Load all data-----
   rv <- reactiveValues()
@@ -639,12 +649,7 @@ server <- function(input, output) {
                                                lowercl = colDef(maxWidth = 70),
                                                uppercl = colDef(maxWidth = 70)
                                              ))
-                               )
-                               
-                               
-                               
-                               
-                        ) 
+                               )) 
       )
     }
     
@@ -703,10 +708,66 @@ server <- function(input, output) {
   
   
   # Export ------------------------------------------------------------------
+
+  output$exp_report_comp <- renderUI({
+    
+    pickerInput("exp_report_comp", label = "Select what to group by in your report",
+                choices = list("Sex" = "sex", 
+                               "Year group" = "schyear", 
+                               "Ethnicity" = "ethnicity",
+                               "IMD Quintile" = "imd_quintile",
+                               "Sexuality" = "sexuality", 
+                               "Child looked after" = "cla",
+                               "Young carer" = "caring", 
+                               "Adopted" = "adopted", 
+                               "Smoker" = "smoke_ever",
+                               "Self-harm" = "selfharm_ever",
+                               "Bullied" = "bullied",
+                               "District" = "District"), 
+                selected = input$comp, multiple = FALSE)
+    
+  })
+  
+  output$exp_report_cat <- renderUI({
+    
+    choices <- rv$data$data %>% 
+      select(input$exp_report_comp) %>% 
+      distinct() %>% 
+      pull(input$exp_report_comp)
+    
+    pickerInput("exp_report_cat", "Select the category from the selected group you are most interested in:",
+                choices = as.character(na.omit(choices)), multiple = FALSE,
+                selected = as.character(na.omit(choices)[1]))
+    
+  })
+  
+  output$exp_report <- downloadHandler(
+    filename = "report.html",
+    content = function(file) {
+      tempReport <- file.path(tempdir(), "test.Rmd")
+      file.copy("test.Rmd", tempReport, overwrite = TRUE)
+      
+      # Set up parameters to pass to Rmd document
+      params <- list(var = input$comp,
+                     cat = input$exp_report_cat)
+      
+      # Knit the document, passing in the `params` list, and eval it in a
+      # child of the global environment (this isolates the code in the document
+      # from the code in this app).
+      show_modal_spinner(text = "Rendering report. Please wait, this should take 1-2 minutes.")
+      rmarkdown::render(tempReport, output_file = file,
+                        params = params,
+                        envir = new.env(parent = globalenv())
+      )
+      remove_modal_spinner() # remove it when done
+      
+      }
+  )
   
   output$export <- renderReactable({
     
     reactable(stats())
+    
     
   })
   
