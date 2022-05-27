1f4142bdc7ca68dba774af367bc4beedd446cad1
vyu <Victor.Yu@hertfordshire.gov.uk>
2022-05-27 17:53:44 +0000

    this is essentially what I did in the modified stats file but it works here.

--- R/explore_mod.R
+++ R/explore_mod.R
@@ -176,6 +176,9 @@ explore_mod_server <- function(id,
               current_plot <- current
             }
             
+              current_plot$response <- as_factor(current$response)
+              current_plot$response <- fct_relevel(current$response, levels = c("low", "medium", "high", "very high"))
+            
             # multicat style plot
             int_plot <- create_multi_plot(df = current_plot,
                                           plot_title = "",
