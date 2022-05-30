84b8971ebf5d09bd50e71dad0c895615586b08e6
vyu <Victor.Yu@hertfordshire.gov.uk>
2022-05-30 12:28:23 +0000

    base functions and compresses

--- R/stats_modify.R
+++ R/stats_modify.R
@@ -2,7 +2,7 @@
 
 
 stats <- readRDS("data-raw/stats.rds")
-walk(names(stats), ~ write_rds(stats[[.x]], paste0("~/projects/yphws_dashboard/data-raw/stats_", .x, ".Rds")))
-write_rds(stats, paste0("~/projects/yphws_dashboard/data-raw/stats_reserve.Rds"))
+walk(names(stats), ~ saveRDS(stats[[.x]], paste0("~/projects/yphws_dashboard/data-raw/stats_", .x, ".eds")))
+saveRDS(stats, paste0("~/projects/yphws_dashboard/data-raw/stats_reserve.Rds"))
 stats[1] -> stats
-write_rds(stats, paste0("~/projects/yphws_dashboard/data-raw/stats.Rds")) ## just sex
+saveRDS(stats, paste0("~/projects/yphws_dashboard/data-raw/stats.Rds")) ## just sex
