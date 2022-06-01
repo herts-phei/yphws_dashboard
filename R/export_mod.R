7223784ce17711a98a613deaabbe27dba33e86d7
ayu <Annie.Yu@hertfordshire.gov.uk>
2022-06-01 15:42:16 +0000

    add year as ar in export mod

--- R/export_mod.R
+++ R/export_mod.R
@@ -38,6 +38,7 @@ export_mod <- function(id,
 # Server ------------------------------------------------------------------
 
 export_mod_server <- function(id,
+                              year,
                               data, 
                               stats_combined,
                               q_coded, 
--- app.R
+++ app.R
@@ -161,6 +161,7 @@ server <- function(input, output) {
   # Export ------------------------------------------------------------------
   
   export_mod_server(id = "export",
+                    year = shiny::reactive(input$year),
                     data = shiny::reactive(rv$data$data),
                     stats_combined = shiny::reactive(rv$stats_combined),
                     q_coded = shiny::reactive(rv$data$q_coded),
