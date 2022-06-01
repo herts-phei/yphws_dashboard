bc3dbac02275848dca6e18b1caae68f326fccf20
ayu <Annie.Yu@hertfordshire.gov.uk>
2022-06-01 10:07:14 +0000

    QA of #60 changes

--- R/export_mod.R
+++ R/export_mod.R
@@ -8,130 +8,51 @@ export_mod <- function(id,
   tablerTabItem(
     tabName = "Export",
     tagList(
-      fluidRow(
-        tablerCard(width = 12, title = "Data table", 
-                   closable = FALSE,
-                   uiOutput(ns("exp_year")), 
-                   uiOutput(ns("exp_breakdown")),
-                   uiOutput(ns("exp_theme")),
-                   uiOutput(ns("exp_question")),
-                   br(),
-                   downloadButton(ns("exp_table"), "Export table"),
-                   br(),
-                   reactableOutput(ns("data_table"))
+      shiny::fluidRow(
+        tablerDash::tablerCard(width = 12, title = "Data table", 
+                               closable = FALSE,
+                               uiOutput(ns("exp_year")), 
+                               uiOutput(ns("exp_breakdown")),
+                               uiOutput(ns("exp_theme")),
+                               uiOutput(ns("exp_question")),
+                               br(),
+                               downloadButton(ns("exp_table"), "Export table"),
+                               br(),
+                               reactableOutput(ns("data_table"))
         )
       ),
-      fluidRow(
-        tablerCard(title = "Export full report (COMING SOON)",
-                   width = 12, 
-                   closable = FALSE#,
-                   # uiOutput(ns("exp_report_comp")),
-                   # uiOutput(ns("exp_report_cat")),
-                   # downloadButton(ns("exp_report"), "Export report")
+      shiny::fluidRow(
+        tablerDash::tablerCard(title = "Export full report(COMING SOON)",
+                               width = 12, 
+                               closable = FALSE,
+                               shiny::uiOutput(ns("exp_report_comp")),
+                               shiny::uiOutput(ns("exp_report_cat")),
+                               shiny::downloadButton(ns("exp_report"), "Export report")
         )
-      )
     )
+  )
   )
   
 }
 
-
-
 # Server ------------------------------------------------------------------
-
-# NOTE: Selecting the Safety health topic will crash the app locally, but not on the server for some reason. 
 
 export_mod_server <- function(id,
                               data, 
                               stats_combined,
                               q_coded, 
-                              comp, 
-                              exp_year, 
-                              exp_breakdown,
-                              exp_theme, 
-                              exp_question, 
-                              export_button) {
+                              comp) {
   
   moduleServer(
     id,
     function(input, output, session) {
       
-      output$exp_report_comp <- renderUI({
-      
       ns <- NS(id)
-      comp <-  input$comp
       
-      pickerInput(
-        inputId = ns("exp_report_comp"), 
-        label = "Select what to group by in your report",
-        choices = list("Sex" = "sex",
-                       "Year group" = "schyear",
-                       "Ethnicity" = "ethnicity",
-                       "IMD Quintile" = "imd_quintile",
-                       "Sexuality" = "sexuality",
-                       "Young carer" = "caring",
-                       # "Smoker" = "smoke_ever",
-                       "Self-harm" = "selfharm_ever",
-                       "Bullied" = "bullied",
-                       "District" = "District"),
-        selected = comp,
-        multiple = FALSE)
-      
-    })
-      
-      output$exp_report_cat <- shiny::renderUI({
-        
-        data <- data()
-        
-        ns <- NS(id)
-        
-        choices <- unique(data[[input$exp_report_comp]]$breakdown) 
-        
-        choices <- choices[choices != "All Responses" & choices != "Non-white"]
-        
-        
-        shinyWidgets::pickerInput(ns("exp_report_cat"), "Select the category from the selected group you are most interested in:",
-                                  choices = as.character(na.omit(choices)), multiple = FALSE,
-                                  selected = as.character(na.omit(choices)[1]))
-        
-      })
-    
-    
-    output$exp_report <- downloadHandler(
+# Table export ------------------------------------------------------------
 
-
-      filename = function() {
-        paste0("Hertfordshire YPHWS Report - ", input$exp_report_comp, " focusing on ", input$exp_report_cat, ".html")
-      },
-
-      content = function(file) {
-
-        shiny::withProgress(message = "Producing the report. This can take some time...", {
-
-          src <- normalizePath('report.Rmd')
-
-          # temporarily switch to the temp dir, in case you do not have write permission to the current working directory
-          owd <- setwd(tempdir())
-          on.exit(setwd(owd))
-          file.copy(src, 'report.Rmd', overwrite = TRUE)
-
-          # Set up parameters to pass to Rmd document
-          params <- list(var = input$exp_report_comp,
-                         cat = input$exp_report_cat,
-                         rendered_by_shiny = TRUE)
-
-
-          out <- rmarkdown::render('report.Rmd', params = params, envir = new.env())
-
-          file.rename(out, file)
-
-        })
-      }
-    )
-    
     output$exp_year <- renderUI({
-      
-      ns <- NS(id)
+
       stats_combined <- stats_combined()
       
       shinyWidgets::awesomeCheckboxGroup(
@@ -148,7 +69,6 @@ export_mod_server <- function(id,
     output$exp_breakdown <- renderUI({
       
       stats_combined <- stats_combined()
-      ns <- NS(id)
       
       shinyWidgets::awesomeCheckboxGroup(
         inputId = ns("exp_breakdown"),
@@ -164,7 +84,6 @@ export_mod_server <- function(id,
     output$exp_theme <- renderUI({
       
       q_coded <- q_coded()
-      ns <- NS(id)
       
       pickerInput(
         inputId = ns("exp_theme"),
@@ -180,7 +99,6 @@ export_mod_server <- function(id,
       
       q_coded <- q_coded()
       exp_theme <- input$exp_theme
-      ns <- NS(id)
       
       filtered <- q_coded %>% 
         mutate(across(where(is.character), ~na_if(., "NA"))) %>% 
@@ -224,64 +142,11 @@ export_mod_server <- function(id,
                question %in% exp_question) 
       
     })
-    
-    #event reactive table
-    # observeEvent(export_button(), {
-    #   
-    #   data <- data()
-    #   comp <- comp()
-    #   q_coded <- q_coded()
-    #   exp_year <- exp_year()
-    #   exp_breakdown <- exp_breakdown()
-    #   exp_theme <- exp_theme()
-    #   exp_question <- exp_question()
-    #   
-    #   #rv$
-    #     table_data <- data[[comp]]  %>%
-    #     left_join(select(q_coded, question_coded, question_theme, survey_text), q_coded,
-    #               by = c("question" = "question_coded")) %>%
-    #     select(year, breakdown, topic = question_theme, question = survey_text,
-    #            `question option` = question_text, response,
-    #            count, denominator, value, lowercl, uppercl) %>%
-    #     distinct() %>%
-    #     filter(year %in% exp_year,
-    #            breakdown %in% exp_breakdown,
-    #            topic %in% exp_theme,
-    #            question %in% exp_question)
-    #   
-    # })
-    
-    # shiny::observeEvent(input$export_button, {
-    eventReactive(export_button(),{
-      
-      data <- data()
-      comp <- comp()
-      q_coded <- q_coded()
-      exp_year <- input$exp_year
-      exp_breakdown <- input$exp_breakdown
-      exp_theme <- input$exp_theme
-      exp_question <- input$exp_question
-      
-      table_data <- data[[comp]]  %>%
-        dplyr::left_join(dplyr::select(q_coded, question_coded, question_theme, survey_text), q_coded,
-                         by = c("question" = "question_coded")) %>%
-        dplyr::select(year, breakdown, topic = question_theme, question = survey_text,
-                      `question option` = question_text, response,
-                      count, denominator, value, lowercl, uppercl) %>%
-        dplyr::distinct() %>%
-        dplyr::filter(year %in% exp_year,
-                      breakdown %in% exp_breakdown,
-                      topic %in% exp_theme,
-                      question %in% exp_question)
-      
-      
-      
-      
-    }) 
 
-        output$data_table <- renderReactable({
+    # render reactable
+    output$data_table <- renderReactable({
       
-        table_data() %>% 
+      table_data() %>% 
         reactable(filterable = TRUE, defaultPageSize = 10, 
                   columns = list(
                     value = colDef(format = colFormat(percent = TRUE, digits = 1)),
@@ -291,11 +156,12 @@ export_mod_server <- function(id,
       
     })
     
+    # download handler
     output$exp_table <- downloadHandler(
       
       filename = "data.csv",
       content = function(con) {
-
+        
         # data <- output$data_table
         data <- table_data()
         write.csv(data, con)
@@ -303,6 +169,76 @@ export_mod_server <- function(id,
       }
       
     ) 
+    
+# Report export -----------------------------------------------------------
+
+    output$exp_report_comp <- renderUI({
+      
+      comp <- comp()
+      
+      pickerInput(
+        inputId = ns("exp_report_comp"), 
+        label = "Select what to group by in your report",
+        choices = list("Sex" = "sex",
+                       "Year group" = "schyear",
+                       "Ethnicity" = "ethnicity",
+                       "IMD Quintile" = "imd_quintile",
+                       "Sexuality" = "sexuality",
+                       "Young carer" = "caring",
+                       # "Smoker" = "smoke_ever",
+                       "Self-harm" = "selfharm_ever",
+                       "Bullied" = "bullied",
+                       "District" = "District"),
+        selected = comp,
+        multiple = FALSE)
+      
+    })
+    
+    output$exp_report_cat <- shiny::renderUI({
+      
+      data <- data()
+      
+      choices <- unique(data[[input$exp_report_comp]]$breakdown) 
+      choices <- choices[choices != "All Responses" & choices != "Non-white"]
+      
+      shinyWidgets::pickerInput(ns("exp_report_cat"), "Select the category from the selected group you are most interested in:",
+                                choices = as.character(na.omit(choices)), multiple = FALSE,
+                                selected = as.character(na.omit(choices)[1]))
+      
+    })
+    
+    # download handler
+    output$exp_report <- downloadHandler(
+      
+      
+      filename = function() {
+        paste0("Hertfordshire YPHWS Report - ", input$exp_report_comp, " focusing on ", input$exp_report_cat, ".html")
+      },
+      
+      content = function(file) {
+        
+        shiny::withProgress(message = "Producing the report. This can take some time...", {
+          
+          src <- normalizePath('report.Rmd')
+          
+          # temporarily switch to the temp dir, in case you do not have write permission to the current working directory
+          owd <- setwd(tempdir())
+          on.exit(setwd(owd))
+          file.copy(src, 'report.Rmd', overwrite = TRUE)
+          
+          # Set up parameters to pass to Rmd document
+          params <- list(var = input$exp_report_comp,
+                         cat = input$exp_report_cat,
+                         rendered_by_shiny = TRUE)
+          
+          
+          out <- rmarkdown::render('report.Rmd', params = params, envir = new.env())
+          
+          file.rename(out, file)
+          
+        })
+      }
+    )
       
     }
   )
