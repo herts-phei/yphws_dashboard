9500c5f8074ba7c652cde015c6162e405c1aabcf
ayu <annie.yu@hertfordshire.gov.uk>
2024-02-21 19:31:14 +0000

    remove manifest files

--- manifest.R
+++
@@ -1,2 +0,0 @@
-rsconnect::writeManifest(appFiles = c("app.R", "R/global_utils.R", "R/key_mod.R", "R/key_utils.R", "R/explore_mod.R", 
-                                      "R/explore_utils.R", "R/inequalities_mod.R", "R/inequalities_utils.R", "www/"))
